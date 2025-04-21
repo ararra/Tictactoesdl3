1ad5ec2c2b65dc4b85d2bd9d039cc949c1c7828a
ararra <ali.alwan0@gmail.com>
2025-04-21 22:18:35 +0200

    implementing mouse event, not finished, has bug in g_clicked_box.

--- main.c
+++ main.c
@@ -33,46 +33,48 @@ bool initialize_game()
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create renderer: %s\n", SDL_GetError());
         return 1;
     }
-    surface = IMG_Load("./assets/Board.png");
-    if (surface == NULL) {
+    g_surface = IMG_Load("./assets/Board.png");
+    if (g_surface == NULL) {
         // In the case that the surface could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create surface: %s\n", SDL_GetError());
         return 1;
     }
 
-    gameboard = SDL_CreateTextureFromSurface(g_renderer, surface);
-    if (gameboard == NULL) {
+    g_gameboard = SDL_CreateTextureFromSurface(g_renderer, g_surface);
+    if (g_gameboard == NULL) {
         // In the case that the gameboard could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create texture gameboard: %s\n", SDL_GetError());
         return 1;
     }    
-    surface = IMG_Load("./assets/X.png");
-    if (surface == NULL) {
+    g_surface = IMG_Load("./assets/X.png");
+    if (g_surface == NULL) {
         // In the case that the surface  X could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create surface X: %s\n", SDL_GetError());
         return 1;
     }
-    X = SDL_CreateTextureFromSurface(g_renderer, surface);
-    if (X == NULL) {
+    g_X = SDL_CreateTextureFromSurface(g_renderer, g_surface);
+    if (g_X == NULL) {
         // In the case that the X could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create texture X: %s\n", SDL_GetError());
         return 1;
     }    
-    surface = IMG_Load("./assets/O.png");
-    if (surface == NULL) {
+    g_surface = IMG_Load("./assets/O.png");
+    if (g_surface == NULL) {
         // In the case that the surface  O could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create surface O: %s\n", SDL_GetError());
         return 1;
     }
-    O = SDL_CreateTextureFromSurface(g_renderer, surface);
-    if (O == NULL) {
+    g_O = SDL_CreateTextureFromSurface(g_renderer, g_surface);
+    if (g_O == NULL) {
         // In the case that the O could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create texture O: %s\n", SDL_GetError());
         return 1;
     }
-    //TODO: Load X and O.
+    
+    //X starts
+    g_turn = 1;
+    
     //TODO: Add start button
-    //TODO: Add mouse click start with X then O
     //TODO: Add win screen
     
     return true;
@@ -100,10 +102,13 @@ void game_loop()
             }
         }  
 
-    
-    //printf("are we here\n");
+    // printf("are we here\n");
     SDL_RenderClear(g_renderer);
-    SDL_RenderTexture(g_renderer, gameboard, NULL, NULL);
+    SDL_RenderTexture(g_renderer, g_gameboard, NULL, NULL);
+    //TODO: Whatever is wrong with this
+    //SDL_RenderTexture(g_renderer, g_O, NULL, g_clicked_box);
+
+
     SDL_RenderPresent(g_renderer);
     // update game state, draw the current frame
 
@@ -128,6 +133,49 @@ void mouse_click_action(SDL_Event event)
     
     float x = event.button.x;
     float y = event.button.y;
+    //find_square(x,y);
+    //TODO: adding values to g_clicked box crashes the game fix it
+    g_clicked_box.w = 50.0;
+    g_clicked_box.h = 50.0;
 
-    printf("loser: %f,%f,\n", x,y);
+
+}
+
+//function to find out what square has been selected
+//TODO: figure out a more elegant way
+void find_square(float x, float y)
+{
+    int position_x = -1;
+    int position_y = -1;
+    //check which column
+    if(x < g_square_x_ends[1])
+    {
+        position_x = 0;
+    }
+    else if (x < g_square_x_ends[2])
+    {
+        position_x = 1;
+    }
+    else
+    {
+        position_x = 2;
+    }
+
+    //check which row
+    if(y < g_square_y_ends[1])
+    {
+        position_y = 0;
+    }
+    else if (y < g_square_y_ends[2])
+    {
+        position_y = 1;
+    }
+    else
+    {
+        position_y = 2;
+    }
+
+    g_clicked_box->x = g_square_x_ends[position_x];
+    g_clicked_box->y = g_square_y_ends[position_y];
+
 }
--- main.h
+++ main.h
@@ -10,14 +10,16 @@
 
 SDL_Window *g_window;
 SDL_Renderer *g_renderer;
-SDL_Surface *surface;
+SDL_Surface *g_surface;
 
 
-SDL_Texture* gameboard;
-SDL_Texture* X;
-SDL_Texture* O;
-
-
+SDL_Texture* g_gameboard;
+SDL_Texture* g_X;
+SDL_Texture* g_O;
+//if turn == 1 then  X
+//if turn == 0 then O
+int g_turn;
+SDL_FRect *g_clicked_box;
 
 bool initialize_game();
 void game_loop();
@@ -27,4 +29,10 @@ bool shutdown_game();
 
 void mouse_click_action(SDL_Event event);
 
-float position[9][2] = {{110.0,75.0},{330,75},{550,75},{110,225},{330,225},{550,225},{110,390},{330,390},{550,390}};
+//float square_middle_pos[9][2] = {{110.0,75.0},{330,75},{550,75},{110,225},{330,225},{550,225},{110,390},{330,390},{550,390}};
+
+//
+float g_square_x_ends[3] = {0.0, 225.0, 440.0};
+float g_square_y_ends[3] = {0.0, 148.0, 303.0};
+
+void find_square(float x, float y);
